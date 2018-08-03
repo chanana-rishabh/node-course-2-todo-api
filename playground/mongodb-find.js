6c9f26e188a3fbd70d0af76b0d118b1d69469bd2
Rishabh Chanana <rish.chanana@gmail.com>
2018-08-03 22:20:34 +0530

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b643f05f5c5e72352897b05')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Rishabh'
+  }).toArray().then((docs) => {
+    console.log('My Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.close();
+});
